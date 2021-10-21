26c5d73c1046f2eff84753627483a25ada60c7b3
MarkBranton1999 <markbranton99@gmail.com>
2021-10-20 22:36:17 -0400

    website layout mostly finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,56 @@
 import Mark from './Mark.png';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Projects from './projects.json'
 
-function App() {
+var Rainbow = require('rainbowvis.js');
+var myRainbow = new Rainbow();
+myRainbow.setSpectrum('skyblue', 'darkcyan', 'darkgreen', 'yellowgreen', 'darkgoldenrod', 'darkorange')
+
+function App(props) {
+
+  const [height, setHeight] = useState(document.documentElement.scrollHeight);
+  const [color, setColor] = useState("#" + myRainbow.colorAt(window.pageYOffset));
+
+  useEffect(() => {
+    function updateColorOnScroll(Offset){
+      return "#" + myRainbow.colorAt(Offset);
+    }
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () =>
+        setColor(updateColorOnScroll(window.pageYOffset))
+      );
+      window.addEventListener("load", () =>
+        setHeight(document.documentElement.scrollHeight),
+        myRainbow.setNumberRange(0, height)
+      );
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header container-fluid" style={{backgroundColor: color}}>
         <img src={Mark} className="App-logo"/>
-        <h3>
-          Hello, my name is Mark Branton and welcome to my website
-        </h3>
-        <p>
+        <h1>Mark Branton</h1>
+        <div>
           I am a 4th year computer engineering student at the University of Waterloo who is always looking for new job opportunities.
-        </p>
+        </div>
+        <div>
+          I am mainly looking for web development jobs.
+          Please do not hesitate to reach out to me with job opportunities.
+        </div>
       </header>
+      <div class="screen container-fluid" style={{backgroundColor: color}}>
+        <h1>
+          Projects
+        </h1>
+        {Projects.map((Project) => {
+          return (<div>
+            <h2>{Project.Title}</h2>
+            <p>{Project.Description}</p>
+          </div>);
+        })}
+      </div>
     </div>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App background_color="#2c3e50"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
